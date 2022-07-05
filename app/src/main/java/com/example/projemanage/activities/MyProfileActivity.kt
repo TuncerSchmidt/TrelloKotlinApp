841d385ed7fbfd11376aaae8ee752bead0d766c9
tuncer akalin <tuncerakalin407@gmail.com>
2022-07-04 23:39:26 -0500

    profile photo add to firebase process completed

--- app/src/main/java/com/example/projemanage/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/MyProfileActivity.kt
@@ -1,18 +1,41 @@
 package com.example.projemanage.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.projemanage.R
 import com.example.projemanage.activities.firebase.FirestoreClass
 import com.example.projemanage.activities.models.User
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
+    companion object{
+        private const val READ_STORAGE_PERMISSION_CODE =1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+    private var mSelectedImageFileUri: Uri? = null
+    private var mProfileImageURL: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
@@ -20,6 +43,52 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this)
+
+        findViewById<ImageView>(R.id.iv_profile_user_image).setOnClickListener{
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_STORAGE_PERMISSION_CODE)
+            }
+        }
+        findViewById<Button>(R.id.btn_update).setOnClickListener{
+            if(mSelectedImageFileUri != null){
+                uploadUserImage()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                Toast.makeText(this, "Ooppss, you just denied the permission for storage", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun showImageChooser(){
+        var galleryIntent = Intent(Intent. ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data != null){
+            mSelectedImageFileUri = data.data
+        }
+        try {
+            Glide.with(this@MyProfileActivity).load(mSelectedImageFileUri).centerCrop()
+                .placeholder(R.drawable.ic_user_place_holder).into(findViewById(R.id.iv_profile_user_image))
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
     }
 
     private fun setupActionBar(){
@@ -35,12 +104,39 @@ class MyProfileActivity : BaseActivity() {
 
     fun setUserDataInUI(user: User) {
         Glide.with(this@MyProfileActivity).load(user.image).centerCrop()
-            .placeholder(R.drawable.ic_user_place_holder).into(findViewById(R.id.iv_user_image))
+            .placeholder(R.drawable.ic_user_place_holder).into(findViewById(R.id.iv_profile_user_image))
         findViewById<AppCompatEditText>(R.id.et_name).setText(user.name)
         findViewById<AppCompatEditText>(R.id.et_email).setText(user.email)
         if(user.mobile != null){
         findViewById<AppCompatEditText>(R.id.et_mobile).setText(user.mobile.toString())
         }
 
+    }
+    private fun uploadUserImage(){
+        showProgressDialog(resources.getString(R.string.please_wait))
+        if(mSelectedImageFileUri != null){
+            val sRef: StorageReference = FirebaseStorage.getInstance().reference.child("USER_IMAGE" + System.currentTimeMillis() + "." + getFileExtension(mSelectedImageFileUri))
+            sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
+                taskSnapshot ->
+                Log.i("Firebase Image URL", taskSnapshot.metadata!!.reference!!.downloadUrl.toString())
+                taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+                    uri ->
+                    Log.i("Downloadable Image URL", uri.toString())
+                    mProfileImageURL = uri.toString()
+                    hideProgressDialog()
+                    //TODO UpdateUserProfileData
+                }
+            }.addOnFailureListener{
+                exception ->
+                Toast.makeText(this@MyProfileActivity, exception.message, Toast.LENGTH_LONG).show()
+                hideProgressDialog()
+            }
+
+        }
+    }
+
+
+    private fun getFileExtension(uri: Uri?): String?{
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
     }
 }
